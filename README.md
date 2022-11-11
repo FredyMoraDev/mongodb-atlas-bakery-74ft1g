0eea4603bb9203230d481a262b006921eca61f48
FredyMoraDev <89317984+FredyMoraDev@users.noreply.github.com>
2022-11-10 21:42:57 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mongodb-atlas-bakery-74ft1g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/mongodb-atlas-bakery-74ft1g)
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  APP_ID: 'bakery-vyjdt',
+  GRAPHQL_URI: 'https://us-central1.gcp.realm.mongodb.com/api/client/v2.0/app/bakery-vyjdt/graphql',
+  API_KEY: 'VeRLX8WvtuYHmcCuproDIq4s4a7JuhCdinez3JGD0zlfli8sAKCohooKxUkp2k6i',
+};
